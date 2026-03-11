7d7f9f3fdf5b768893198ce274d9258a29751e44
Kavy Pujara <kavypujara@example.com>
2026-03-11 12:00:46 +0530

    fix: wildcard routing for express 5 compatibility

--- backend/server.js
+++ backend/server.js
@@ -96,7 +96,7 @@ app.use((err, req, res, next) => {
 // Serve frontend in production
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../dist')));
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
