7721940179b7fbd45939e9c0906faceacb7dab52
Kavy Pujara <kavypujara@example.com>
2026-03-11 11:43:17 +0530

    chore: fix render deployment configuration

--- backend/server.js
+++ backend/server.js
@@ -93,6 +93,13 @@ app.use((err, req, res, next) => {
     });
 });
 
+// Serve frontend in production
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
